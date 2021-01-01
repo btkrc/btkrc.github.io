2198ec4829e309ca62ca08a7e013ad81178356a5
btKrc1nx <33425842@qq.com>
2021-01-01 18:51:53 +0800

    Site updated: 2021-01-01 18:51:51

--- uploadsFile/encodeUrl.js
+++ uploadsFile/encodeUrl.js
@@ -1,11 +1,15 @@
-jQuery(function () {
-    $('#encodeUrl').on('click', function (ev) {
-        var urlText = $('#urlText').val();
+function func1(ev){
+      var urlText = $('#urlText').val();
         $('#outUrl').text(encodeURIComponent(urlText));
-    })
+}
 
-    $('#decodeUrl').on('click', function (ev) {
+function func2(ev){
         var urlText = $('#urlText').val();
         $('#outUrl').text(decodeURIComponent(urlText));
-    })
-})
+}
+
+    $('#encodeUrl').off('click', func1);
+    $('#decodeUrl').off('click', func2);
+
+    $('#encodeUrl').on('click', func1);
+    $('#decodeUrl').on('click', func2);
