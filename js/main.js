b66d2fceeac4c48645b5ec69a2cf5e968cddcb25
btKrc1nx <33425842@qq.com>
2021-02-10 04:34:55 +0800

    Site updated: 2021-02-10 04:34:52

--- js/main.js
+++ js/main.js
@@ -641,7 +641,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const $runtimeCount = document.getElementById('runtimeshow')
     if ($runtimeCount) {
       const publishDate = $runtimeCount.getAttribute('data-publishDate')
-      $runtimeCount.innerText = btf.diffDate(publishDate) + ' ' + GLOBAL_CONFIG.runtime
+      $runtimeCount.innerText = '本站已运行 ' + btf.diffDate(publishDate) + ' ' + GLOBAL_CONFIG.runtime
     }
   }
 
